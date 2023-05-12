1677d68add7915c79fd2fefa8e4f788e51d04ddc
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2023-05-12 16:56:19 +0200

    next - connect database

--- flask/model.py
+++ flask/model.py
@@ -1,11 +1,21 @@
 from pydantic import BaseModel
 
 
-class Token(BaseModel):
-    tokenstring = str
+class NextflowUser(BaseModel):
+    identifier_token: str
 
     def to_bson(self):
         data = self.dict(by_alias=True, exclude_none=True)
-        if data["_id"] is None:
-            data.pop("_id")
+        #if data["_id"] is None:
+        #    data.pop("_id")
+        return data
+
+
+class NextflowRunToken(BaseModel):
+    run_identifier_token: str
+
+    def to_bson(self):
+        data = self.dict(by_alias=True, exclude_none=True)
+        #if data["_id"] is None:
+        #    data.pop("_id")
         return data
--- flask/server.py
+++ flask/server.py
@@ -1,22 +1,30 @@
 from flask import Flask, jsonify, request
 # from flask_cors import CORS
-from model import Token
+from model import NextflowRunToken, NextflowUser
+from pymongo import MongoClient
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
+client = MongoClient(host='0.0.0.0', port=8005)
+db = client.flask_db
+
+tokens = db.nextflowruntokens
 
 # enable CORS
 #CORS(app, resources={r'/*': {'origins': '*'}})
 
 
+
 # sanity check route
 @app.route('/ping', methods=['GET', 'POST'])
 def ping_pong():
-    token = Token(**request.args)
-    return jsonify(token.json())
+    token = NextflowRunToken(**request.args)
+    tokens.insert_one(token.to_bson())
+
+    all_tokens = tokens.find()
+    return jsonify(all_tokens)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000, debug=True)
-
+    app.run(host='0.0.0.0', port=8000, debug=True)
