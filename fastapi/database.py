68c79f82e471051072242c6feb2f3c43fd5631d9
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2023-06-09 17:49:19 +0200

    further work

--- fastapi/database.py
+++ fastapi/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pgpassword1@localhost:5432/nextflow_analysis"
+# adjust to host env variable
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pgpassword1@nfa_db:5432/nextflow_analysis"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
