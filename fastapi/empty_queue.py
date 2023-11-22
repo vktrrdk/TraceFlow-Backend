da869f3a4d99167f977f4da779209587a131073e
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2023-11-22 11:28:02 +0000

    added script for queue emptying

---
+++ fastapi/empty_queue.py
@@ -0,0 +1,12 @@
+from redis import Redis
+import os
+from rq import Worker, Queue, Connection
+import logging
+
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+r_con = Redis(host=REDIS_HOST, port=6379)
+
+if __name__ == '__main__':
+    queue = Queue("request_queue", connection=r_con)
+    queue.empty()
